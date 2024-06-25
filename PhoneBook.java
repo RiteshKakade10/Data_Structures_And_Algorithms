794569b94aa5c141153c6a4d605d6647c19d618b
Ritesh Kakade <127117043+RiteshKakade10@users.noreply.github.com>
2024-06-25 10:47:10 +0530

    Add files via upload

---
+++ PhoneBook.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+class PhoneBook{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        Map<String,Integer> phonebook=new HashMap<>();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            phonebook.put(name,phone);
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            if(phonebook.containsKey(s))
+            {
+                System.out.println(s+"="+phonebook.get(s));
+            }
+            else{
+                System.out.println("Not found");
+            }
+        }
+        in.close();
+    }
+}
