dcd92f5dffe24e9e34ad39e3fd0b7a3fcc75c40a
Ritesh Kakade <127117043+RiteshKakade10@users.noreply.github.com>
2024-07-10 16:16:04 +0530

    Add files via upload

---
+++ KadanesAlgo.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class KadanesAlgo
+{
+	public static int MaxSubArray(int arr1[])
+	{
+			int cs=0;
+			int ms=Integer.MIN_VALUE;
+			for(int i=0;i<arr1.length;i++)
+			{
+				cs+=arr1[i];
+				if(cs<0)
+				{
+					cs=0;
+				}
+				ms=Math.max(ms,cs);
+			}
+		return ms;
+	}
+	public static void main(String[] args) {
+		int arr1[]={1,2,3,4,5,6,7,8,9,10};
+		System.out.println("Maximum Sum Subarray Is:"+MaxSubArray(arr1));
+	}
+}
