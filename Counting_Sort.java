0c35931d34b05e826fb82bd96f51f820c3555d3e
Ritesh Kakade <127117043+RiteshKakade10@users.noreply.github.com>
2024-10-27 17:44:02 +0530

    Create Counting_Sort.java
    [Problem] Counting Sort

---
+++ Counting_Sort.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Counting_Sort {
+
+    public static List<Integer> countingSort(List<Integer> arr) {
+    List<Integer> arr1=new  ArrayList<Integer>(100);
+    for(int i=0;i<=99;i++) {
+        arr1.add(0);
+        }
+    for(int i=0;i<arr1.size();i++){
+        arr1.set(arr.get(i),arr1.get(arr.get(i))+1);
+        }
+    
+    return arr1;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.countingSort(arr);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
